39485cb01449ef0a3e8ad6a73c5ed6eca6fb0d88
Jordan Brown <jordan.brown@connecthealthcare.com>
2016-09-28 11:24:55 -0400

    Add exception handling

---
+++ TaskExtensions.Tests/ExceptionHandlingTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TaskExtensions.Tests
+{
+	[TestFixture]
+	public class ExceptionHandlingTests
+	{
+		[Test]
+		public void Can_Handle_Exceptions()
+		{
+			Func<int> alwaysFails = () =>
+			{
+				throw new OverflowException();
+			};
+
+			var task = Task.Run(alwaysFails)
+				.Except()
+				.On<OverflowException>(() => 2);
+			Assert.AreEqual(2, task.Result);
+		}
+
+		[Test]
+		public void Other_Exceptions_Pass_Through()
+		{
+			Func<int> alwaysFails = () =>
+			{
+				throw new InvalidOperationException();
+			};
+
+			var task = Task.Run(alwaysFails)
+				.Except()
+				.On<OverflowException>(() => 2);
+			Assert.Throws<AggregateException>(() => task.Wait());
+		}
+
+		[Test]
+		public void Can_Replace_Canceled_Tasks()
+		{
+			var ct = new CancellationToken(canceled: true);
+			var canceled = Task.Run(() => 1, ct);
+			var task = canceled
+				.Except()
+				.WhenCanceled(() => 2);
+			Assert.AreEqual(2, task.Result);
+		}
+	}
+}
---
+++ TaskExtensions/ExceptionHandling.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExtensions
+{
+	public static class ExceptionHandling
+	{
+		public static ExceptionHandler<TResult> Except<TResult>(
+			this Task<TResult> source)
+		{
+			return new ExceptionHandler<TResult>(source);
+		}
+	}
+
+	public class ExceptionHandler<TResult>
+	{
+		private readonly Task<TResult> Source;
+		public ExceptionHandler(Task<TResult> source)
+		{
+			Source = source;
+		}
+
+		public Task<TResult> WhenCanceled(Func<TResult> replacement)
+		{
+			return Source.ContinueWith(s =>
+			{
+				if (s.IsCanceled)
+				{
+					return replacement();
+				}
+
+				return s.Result;
+			});
+		}
+
+		public Task<TResult> WhenCanceled(Func<Task<TResult>> replacement)
+		{
+			return Source.ContinueWith(s =>
+			{
+				if (s.IsCanceled)
+				{
+					return replacement();
+				}
+
+				return s;
+			}).Unwrap();
+		}
+
+		public Task<TResult> On<TException>(Func<Task<TResult>> replacement)
+			where TException : Exception
+		{
+			return Source.ContinueWith(s =>
+			{
+				if (s.Exception != null
+					&& s.Exception.InnerException is TException)
+				{
+						return replacement();
+				}
+
+				return s;
+			}).Unwrap();
+		}
+
+		public Task<TResult> On<TException>(Func<TResult> replacement)
+			where TException : Exception
+		{
+			return Source.ContinueWith(s =>
+			{
+				if (s.Exception != null
+					&& s.Exception.InnerException is TException)
+				{
+					return replacement();
+				}
+
+				return s.Result;
+			});
+		}
+	}
+}
