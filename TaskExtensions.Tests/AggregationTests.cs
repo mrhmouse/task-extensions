56ca08188cdabc0a7247cc9c539148c0dfd4ef7f
Jordan Brown <jordan.brown@connecthealthcare.com>
2016-09-28 12:01:32 -0400

    Add aggregation functions

---
+++ TaskExtensions.Tests/AggregationTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TaskExtensions.Linq;
+
+namespace TaskExtensions.Tests
+{
+	[TestFixture]
+	public class AggregationTests
+	{
+		[Test]
+		public void Canceled_Tasks_Cant_Be_Concatenated()
+		{
+			var ct = new CancellationToken(canceled: true);
+			var one = Task.Run(() => 1, ct);
+			var two = Task.FromResult(2);
+			var pair = one.And(two);
+			var exception = Assert.Catch<AggregateException>(() => pair.Wait());
+			Assert.IsInstanceOf<TaskCanceledException>(exception.InnerException);
+		}
+
+		[Test]
+		public void Tasks_Can_Be_Concatenated()
+		{
+			var one = Task.FromResult(1);
+			var two = Task.FromResult(2);
+			var sum =
+				from pair in one.And(two)
+				select pair[0] + pair[1];
+			Assert.AreEqual(3, sum.Result);
+		}
+
+		[Test]
+		public void Task_Lists_Can_Be_Appended_To()
+		{
+			var nums = Task.FromResult(new[] { 1, 2, 3, 4 });
+			var last = Task.FromResult(5);
+			var task = nums.And(last);
+			Assert.AreEqual(5, task.Result.Last());
+		}
+
+		[Test]
+		public void Task_Lists_Of_Lists_Can_Be_Appended_To()
+		{
+			var nums = Task.FromResult(new[] {
+				new[] { 1, 2, 3, 4 }
+			});
+			var last = Task.FromResult(new[] { 5 });
+			var task = nums.And(last);
+			Assert.AreEqual(5, task.Result.Last().Last());
+		}
+
+		[Test]
+		public void Task_Lists_Of_Lists_Can_Be_Concatenated()
+		{
+			var nums = Task.FromResult(new[] {
+				new[] { 1, 2, 3, 4 }
+			});
+			var last = Task.FromResult(new[] {
+				new[] { 5 }
+			});
+			var task = nums.And(last);
+			Assert.AreEqual(5, task.Result.Last().Last());
+		}
+
+		[Test]
+		public void Task_Lists_Can_Be_Concatenated()
+		{
+			var nums = Task.FromResult(new[] { 1, 2, 3, 4 });
+			var tail = Task.FromResult(new[] { 5, 6, 7, 8 });
+			var task = nums.And(tail);
+			Assert.AreEqual(8, task.Result.Last());
+		}
+	}
+}
---
+++ TaskExtensions/AggregationForTasks.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using TaskExtensions.Linq;
+
+namespace TaskExtensions
+{
+	public static class AggregationForTasks
+	{
+		public static Task<T[]> And<T>(
+			this Task<T> source,
+			Task<T> other)
+		{
+			return Task.WhenAll(source, other);
+		}
+
+		public static Task<T[]> And<T>(
+			this Task<T[]> source,
+			Task<T> other)
+		{
+			return
+				from list in source
+				from tail in other
+				select list.Concat(new[] { tail }).ToArray();
+		}
+
+		public static Task<T[]> And<T>(
+			this Task<T[]> source,
+			Task<T[]> other)
+		{
+			return
+				from list in source
+				from tail in other
+				select list.Concat(tail).ToArray();
+		}
+	}
+}
