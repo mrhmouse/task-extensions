482814d0fa5589a2713ac535e640b85d9ceb921b
Jordan Brown <jordan.brown@connecthealthcare.com>
2016-09-28 10:41:59 -0400

    Initial commit

---
+++ TaskExtensions.Tests/LinqTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using TaskExtensions.Linq;
+
+namespace TaskExtensions.Tests
+{
+	[TestFixture]
+	public class LinqTests
+	{
+		[Test(Description = "LINQ 'select' syntax works")]
+		public void Select_Syntax_Works()
+		{
+			var projected =
+				from x in Task.FromResult(2)
+				select x * 2;
+			Assert.AreEqual(4, projected.Result);
+		}
+
+		[Test(Description = "LINQ chained 'select' syntax works")]
+		public void Chained_Select_Syntax_Works()
+		{
+			var projected =
+				from x in Task.FromResult(2)
+				from y in Task.FromResult(x)
+				select x * y;
+			Assert.AreEqual(4, projected.Result);
+		}
+
+		[Test(Description = "LINQ nested 'select' syntax works")]
+		public void Nested_Select_Syntax_Works()
+		{
+			var projected =
+				from x in Task.FromResult(2)
+				select
+					(from y in Task.FromResult(3)
+					 select x + y);
+			Assert.AreEqual(5, projected.Result);
+		}
+	}
+}
---
+++ TaskExtensions/Linq/LinqForTasks.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExtensions.Linq
+{
+	/// <summary>
+	/// Provides 'select' methods for tasks, without async/await.
+	/// </summary>
+	public static class LinqForTasks
+	{
+		/// <summary>
+		/// When the task has completed, project its result into a new task.
+		/// </summary>
+		/// <typeparam name="TSource">
+		/// The underlying type of the source task.
+		/// </typeparam>
+		/// <typeparam name="TResult">
+		/// The type returned by the projection.
+		/// </typeparam>
+		/// <param name="source">
+		/// The source task.
+		/// </param>
+		/// <param name="projection">
+		/// The projection to make on the result of the task.
+		/// </param>
+		/// <returns>
+		/// A new task.
+		/// </returns>
+		public static Task<TResult> Select<TSource, TResult>(
+			this Task<TSource> source,
+			Func<TSource, TResult> projection)
+		{
+			return source.ContinueWith(s => projection(s.Result));
+		}
+
+		/// <summary>
+		/// When the task has completed, project its result into a new task,
+		/// automatically unwrapping the nested tasks.
+		/// </summary>
+		/// <typeparam name="TSource">
+		/// The underlying type of the source task.
+		/// </typeparam>
+		/// <typeparam name="TResult">
+		/// The underlying type of the task returned by the projection.
+		/// </typeparam>
+		/// <param name="source">
+		/// The source task.
+		/// </param>
+		/// <param name="projection">
+		/// The projection to make on the result of the task.
+		/// </param>
+		/// <returns>
+		/// A new task.
+		/// </returns>
+		public static Task<TResult> Select<TSource, TResult>(
+			this Task<TSource> source,
+			Func<TSource, Task<TResult>> projection)
+		{
+			return source.ContinueWith(s => projection(s.Result)).Unwrap();
+		}
+
+		/// <summary>
+		/// When the task has completed, project its result into a new task,
+		/// unwrapping the nested tasks.
+		/// </summary>
+		/// <typeparam name="TSource">
+		/// The underlying type of the source task.
+		/// </typeparam>
+		/// <typeparam name="TResult">
+		/// The underlying type of the task returned by the projection.
+		/// </typeparam>
+		/// <param name="source">
+		/// The source task.
+		/// </param>
+		/// <param name="projection">
+		/// The projection to make on the result of the task.
+		/// </param>
+		/// <returns>
+		/// A new task.
+		/// </returns>
+		public static Task<TResult> SelectMany<TSource, TResult>(
+			this Task<TSource> source,
+			Func<TSource, Task<TResult>> projection)
+		{
+			return source.ContinueWith(s => projection(s.Result)).Unwrap();
+		}
+
+		/// <summary>
+		/// When the initial task has completed, project its result into a new task.
+		/// When the intermediate task has completed, project its result into a
+		/// new task as well, unwrapping the nested tasks.
+		/// </summary>
+		/// <typeparam name="TSource">
+		/// The underlying type of the source task.
+		/// </typeparam>
+		/// <typeparam name="TMiddle">
+		/// The underlying type of the task returned by the intermediate projection.
+		/// </typeparam>
+		/// <typeparam name="TResult">
+		/// The type returned by the projection.
+		/// </typeparam>
+		/// <param name="source">
+		/// The source task.
+		/// </param>
+		/// <param name="intermediate">
+		/// The intermediate projection.
+		/// </param>
+		/// <param name="projection">
+		/// The final projection to make on the result of the task.
+		/// </param>
+		/// <returns>
+		/// A new task.
+		/// </returns>
+		public static Task<TResult> SelectMany<TSource, TMiddle, TResult>(
+			this Task<TSource> source,
+			Func<TSource, Task<TMiddle>> intermediate,
+			Func<TSource, TMiddle, TResult> projection)
+		{
+			return source
+				.SelectMany(s => intermediate(s).Select(i => projection(s, i)));
+		}
+
+		/// <summary>
+		/// When the initial task has completed, project its result into a new task.
+		/// When the intermediate task has completed, project its result into a
+		/// new task as well, automatically unwrapping the nested tasks.
+		/// </summary>
+		/// <typeparam name="TSource">
+		/// The underlying type of the source task.
+		/// </typeparam>
+		/// <typeparam name="TMiddle">
+		/// The underlying type of the task returned by the intermediate projection.
+		/// </typeparam>
+		/// <typeparam name="TResult">
+		/// The underlying type of the task returned by the projection.
+		/// </typeparam>
+		/// <param name="source">
+		/// The source task.
+		/// </param>
+		/// <param name="intermediate">
+		/// The intermediate projection.
+		/// </param>
+		/// <param name="projection">
+		/// The final projection to make on the result of the task.
+		/// </param>
+		/// <returns>
+		/// A new task.
+		/// </returns>
+		public static Task<TResult> SelectMany<TSource, TMiddle, TResult>(
+			this Task<TSource> source,
+			Func<TSource, Task<TMiddle>> intermediate,
+			Func<TSource, TMiddle, Task<TResult>> projection)
+		{
+			return source
+				.SelectMany(s => intermediate(s).Select(i => projection(s, i)));
+		}
+	}
+}
